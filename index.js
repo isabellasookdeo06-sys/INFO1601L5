4317986a08f97c62abc5b7712b9b696166f4017b
Isabella Sookdeo <isabella.sookdeo06@gmail.com>
2026-03-09 14:42:00 -0400

    Task 2: Created variables and calculated circle area

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let radius = 7;
+const pi = 3.14;
+
+let area = radius * radius * pi;
+console.log("Area of the circle:", area);
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
