8657ae75c673448a704275d406399d327a9581a3
Isabella Sookdeo <isabella.sookdeo06@gmail.com>
2026-03-09 16:26:55 -0400

    Task 4.3: Implemented fizzbuzz loop

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log('1' === 1); //false
-console.log(1 === true); //false
-console.log("true" === true); //false
+for(let i=0; i< 50; i++){
+  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
+    console.log('fizzbuzz');
+  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
+    console.log('fizz');
+  }else if(i%5 === 0){ //no? what about just 5 then?
+    console.log('buzz');
+  }else{              //so its not divisible by 3 or 5 then
+    console.log(i);
+  }
+}
